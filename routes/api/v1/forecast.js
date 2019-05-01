fc71e4267755c4fea2e5763a15ee7b1a80bd39b8
nagerz <znager@gmail.com>
2019-04-30 22:44:36 -0600

    established find or create query by searhc location in forecast route

--- routes/api/v1/forecast.js
+++ routes/api/v1/forecast.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 var User = require('../../../models').User;
 var pry = require('pryjs')
+//eval(pry.it)
 
 router.get("/", function(req, res, next) {
   if (req.body.api_key){
@@ -13,8 +14,20 @@ router.get("/", function(req, res, next) {
       if(!user){
         res.setHeader("Content-Type", "application/json");
         res.status(401).send({ error: "unauthorized" });
+      }else if(!req.query.location){
+        res.setHeader("Content-Type", "application/json");
+        res.status(400).send({ error: "invalid search location" });
       }else{
         eval(pry.it)
+
+
+        Query.findOrCreate({query: req.query.location}).success(function(location, created){
+          if(created){
+            eval(pry.it)
+          }else{
+            console.log("query not created")
+          };
+        });
       }
     });
   }else{
